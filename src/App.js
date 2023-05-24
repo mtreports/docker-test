f3bd6e5ed9b3763006e6abdc01e7fb048a5aec73
mtreports <mt.reports@mandasadevelopment.com>
2023-05-24 13:10:16 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          Well come in Mt reports
         </a>
       </header>
     </div>
